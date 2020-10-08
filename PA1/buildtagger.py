6fdf2e7a0b806abdf5b39c1c2af16b2a7f55dc7d
lws803 <lws803@gmail.com>
2020-10-08 12:10:36 +0800

    tagger with AO smoothing

--- PA1/buildtagger.py
+++ PA1/buildtagger.py
@@ -4,9 +4,50 @@ import sys
 import datetime
 from collections import defaultdict
 import pickle
+import numpy as np
+
 
 UNK = '<UNK>'
 unk_words_threshold = 0
+
+
+def ao_smoothing(
+    pos_count, pos_bigrams, word_pos_pair, word_count, pos_index, word_index
+):
+    word_emission_probabilities = np.zeros((len(pos_index), len(word_index)))
+    transition_probabilities = np.zeros((len(pos_index), len(pos_index)))
+
+    for k, v in pos_bigrams.items():
+        prev_pos = k[0]
+        prev_pos_index = pos_index[k[0]]
+        curr_pos_index = pos_index[k[1]]
+        transition_probabilities[prev_pos_index][curr_pos_index] = (
+            (v + 1) / (
+                len(pos_count) * 1 + pos_count[prev_pos]
+            )
+        )
+    for k, v in word_pos_pair.items():
+        pos = k[0]
+        curr_pos_index = pos_index[k[0]]
+        curr_word_index = word_index[k[1]]
+        word_emission_probabilities[curr_pos_index][curr_word_index] = (
+            (v + 1) / (
+                len(pos_count) * 1 + pos_count[pos]
+            )
+        )
+
+    # Acc for unk words
+    for pos in pos_count.keys():
+        curr_pos_index = pos_index[pos]
+        unknown_word_index = word_index[UNK]
+        word_emission_probabilities[curr_pos_index][unknown_word_index] = (
+            1 / (
+                len(pos_count) * 1 + pos_count[pos]
+            )
+        )
+    # TODO: If transition path is 0 then we shd be ignoring it in viterbi calculation
+    return transition_probabilities, word_emission_probabilities
+
 
 def train_model(train_file, model_file):
     pos_bigrams = defaultdict(int)
@@ -16,7 +57,6 @@ def train_model(train_file, model_file):
     output_word_count = defaultdict(int)
     pos_bigram_types = defaultdict(set)
     word_pos_pair_types = defaultdict(set)
-    # FIXME: Calculate the transition probability and word emission probability here
 
     with open(train_file) as f:
         lines = f.readlines()
@@ -41,20 +81,31 @@ def train_model(train_file, model_file):
 
                 pos_bigrams[(prev_pos, pos)] += 1
                 pos_count[pos] += 1
-                if (word_count[term] <= unk_words_threshold):
-                    word_pos_pair[(pos, UNK)] += 1
-                    word_pos_pair_types[pos].add(UNK)
-                    output_word_count[UNK] += 1
-                else:
-                    word_pos_pair[(pos, term)] += 1
-                    output_word_count[term] += 1
-                    word_pos_pair_types[pos].add(term)
+
+                # Remove unk words for now
+                # if (word_count[term] <= unk_words_threshold):
+                #     term = UNK
+                word_pos_pair[(pos, term)] += 1
+                output_word_count[term] += 1
+                word_pos_pair_types[pos].add(term)
+
                 pos_bigram_types[prev_pos].add(pos)
 
                 prev_pos = pos
         pos_bigrams[(prev_pos, '</s>')] += 1
         pos_bigram_types[prev_pos].add('</s>')
         pos_count['</s>'] += 1
+        pos_index = {}
+        word_index = {UNK: len(output_word_count)}
+
+        for index, val in enumerate(pos_count.keys()):
+            pos_index[val] = index
+        for index, val in enumerate(output_word_count.keys()):
+            word_index[val] = index
+
+        transition_probabilities, word_emission_probabilities = ao_smoothing(
+            pos_count, pos_bigrams, word_pos_pair, output_word_count, pos_index, word_index
+        )
 
     with open(model_file, 'wb') as f:
         pickle.dump({
@@ -63,7 +114,11 @@ def train_model(train_file, model_file):
             'pos_count': dict(pos_count),
             'word_count': dict(output_word_count),
             'pos_bigram_types': {kv[0]: len(kv[1]) for kv in pos_bigram_types.items()},
-            'word_pos_pair_types': {kv[0]: len(kv[1]) for kv in word_pos_pair_types.items()}
+            'word_pos_pair_types': {kv[0]: len(kv[1]) for kv in word_pos_pair_types.items()},
+            'word_index': word_index,
+            'pos_index': pos_index,
+            'transition_probabilities': transition_probabilities,
+            'word_emission_probabilities': word_emission_probabilities,
         }, f)
 
     print('Finished...')
