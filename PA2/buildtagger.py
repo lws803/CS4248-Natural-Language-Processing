a9e16017e9ee59f22543b28d3ae08cdd50572120
Wilson Ler <lws803@gmail.com>
2020-10-24 00:26:24 +0800

    Padded cnn with word batches (#7)
    * added character batches
    
    * run in gpu
    
    * added some todos
    
    * added relu unit
    
    * added zero_grad
    
    * changed padding to 30
    
    * changed some parameters
    
    * moved parameters to global definitions

--- PA2/buildtagger.py
+++ PA2/buildtagger.py
@@ -4,29 +4,36 @@ import os
 import math
 import sys
 from collections import defaultdict
+import datetime
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
+DEVICE = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+EPOCHS = 2  # See if we really need to run 2 epochs
+WORD_EMBEDDINGS_SIZE = 10
+CHAR_EMBEDDINGS_SIZE = 5
+LSTM_HIDDEN_SIZE = 256
 
 class CharCNN(nn.Module):
     def __init__(self, hidden_size, l=3, k=3):
         super(CharCNN, self).__init__()
         self.hidden_size = hidden_size
         self.embedding = nn.Embedding(128, self.hidden_size)
+        self.relu = nn.ReLU()
         self.conv1 = nn.Conv1d(hidden_size, l, kernel_size=k, padding=k)
         self.pool = nn.AdaptiveMaxPool1d(1)
 
-    def forward(self, input_chars):
+    def forward(self, input_chars_list):
         # TODO: Consider adding dropout layer here
-        output = self.embedding(input_chars)
-        # output = torch.transpose(output_batches, 2, 1)  # for list of words
-        output = torch.transpose(output, 0, 1).unsqueeze(0)
-        output = self.conv1(output)
+        output = self.embedding(input_chars_list)
+        output = torch.transpose(output, 2, 1)  # for list of words
+        # output = torch.transpose(output, 0, 1)
+        output = self.relu(self.conv1(output))
         output = self.pool(output)
-        return torch.flatten(output)
+        return output.squeeze(2)
 
 
 class BiLSTM(nn.Module):
@@ -40,20 +47,20 @@ class BiLSTM(nn.Module):
         self.ix_to_word_chars = ix_to_word_chars
 
         self.embedding = nn.Embedding(vocab_size, self.word_embed_size)
-        self.char_cnn = CharCNN(char_embed_size, l=char_embed_size, k=5)
+        self.char_cnn = CharCNN(char_embed_size, l=char_embed_size, k=char_embed_size)
         self.bilstm = nn.LSTM(
             word_embed_size + char_embed_size, lstm_hidden_size, bidirectional=True
         )
-        self.linear = nn.Linear(lstm_hidden_size * 2 ,tag_size)
+        self.linear = nn.Linear(lstm_hidden_size * 2, tag_size)
 
     def forward(self, input_words):
-        output = self.embedding(torch.tensor(input_words, dtype=torch.long))
-        char_embeddings = torch.empty((0, self.char_embed_size))
-        # TODO: This might be expensive operaiton
-        for word_ix in input_words:
-            chars = self.ix_to_word_chars[word_ix]
-            char_embedding = self.char_cnn(chars)  # Merge this together with the word embeddingss
-            char_embeddings = torch.cat((char_embeddings, char_embedding.unsqueeze(0)))
+        output = self.embedding(torch.tensor(input_words, dtype=torch.long, device=DEVICE))
+        # FIXME: Handle unknown words
+        list_of_word_chars = torch.tensor(
+            [self.ix_to_word_chars[idx] for idx in input_words], dtype=torch.long,
+            device=DEVICE
+        )
+        char_embeddings = self.char_cnn(list_of_word_chars)
         output = torch.cat((output, char_embeddings), 1)
         hidden, _ = self.bilstm(output.unsqueeze(1))
         hidden_reformatted = hidden.view(len(input_words), -1)
@@ -98,32 +105,42 @@ def train_model(train_file, model_file):
     for i, term in enumerate(term_count.keys()):
         word_to_ix[term] = i
         ix_to_word[i] = term
-        ix_to_word_chars[i] = torch.tensor(
-            [ord(character) for character in term], dtype=torch.long
-        )
+        word_chars = [ord(character) for character in term]
+        # Padded words
+        ix_to_word_chars[i] = word_chars[0:30] + [0 for i in range(30 - len(word_chars))]
     for i, pos in enumerate(pos_count.keys()):
         pos_to_ix[pos] = i
         ix_to_pos[i] = pos
 
     # TODO: Training, remember to split the training set first
-    bilstm = BiLSTM(10, 10, 256, len(word_to_ix), len(pos_to_ix), ix_to_word_chars)
+    bilstm = BiLSTM(
+        WORD_EMBEDDINGS_SIZE, CHAR_EMBEDDINGS_SIZE, LSTM_HIDDEN_SIZE,
+        len(word_to_ix), len(pos_to_ix), ix_to_word_chars
+    )
+    bilstm.to(DEVICE)
+
     loss_function = nn.NLLLoss()
     optimizer = optim.Adam(bilstm.parameters())
     final_loss = None
-    for index, (words, tags) in enumerate(zip(sentences[0: 500], sentence_tags[0: 500])):
-        # FIXME: If unk words, we will initialize an embedding vector of zeroes
-        tag_scores = bilstm([word_to_ix[word] for word in words])
-        loss = loss_function(tag_scores, torch.tensor([pos_to_ix[tag] for tag in tags]))
-        loss.backward()
-        print(loss, index/500)
-        optimizer.step()
-        final_loss = loss
+    for i in range(EPOCHS):
+        epoch_loss = None
+        for index, (words, tags) in enumerate(zip(sentences, sentence_tags)):
+            bilstm.zero_grad()
+
+            tag_scores = bilstm([word_to_ix[word] for word in words])
+            loss = loss_function(tag_scores, torch.tensor(
+                [pos_to_ix[tag] for tag in tags], device=DEVICE
+            ))
+            loss.backward()
+            optimizer.step()
+            epoch_loss = loss
+        print(epoch_loss)
+        final_loss = epoch_loss
     print(final_loss)
     prediction = bilstm(
         [word_to_ix[word] for word in 'hello world how are you doing today ?'.split()]
     )
     print([ix_to_pos[tag.item()] for tag in torch.argmax(prediction, dim=1)])
-    import pdb; pdb.set_trace()
     print('Finished...')
 
 
@@ -131,4 +148,7 @@ if __name__ == "__main__":
     # make no changes here
     train_file = sys.argv[1]
     model_file = sys.argv[2]
+    start_time = datetime.datetime.now()
     train_model(train_file, model_file)
+    end_time = datetime.datetime.now()
+    print('Time:', end_time - start_time)
